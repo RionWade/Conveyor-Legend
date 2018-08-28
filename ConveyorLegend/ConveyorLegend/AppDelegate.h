e431701682ac9937b047d58d413b1c3ea7dfe6d6
RionWade <42757538+RionWade@users.noreply.github.com>
2018-08-28 11:06:23 +0800

    Add files via upload

---
+++ ConveyorLegend/ConveyorLegend/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ConveyorLegend
+//
+//  Created by Tench Harold on 2018/8/28.
+//  Copyright © 2018年 ConveyorLegend. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ConveyorLegend/ConveyorLegend/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ConveyorLegend
+//
+//  Created by Tench Harold on 2018/8/28.
+//  Copyright © 2018年 ConveyorLegend. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
